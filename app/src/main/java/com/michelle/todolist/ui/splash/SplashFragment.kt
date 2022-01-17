a6a04c4741abc5fad29587446a33508f51ad5f0c
michellebritoo <michele.brito17@gmail.com>
2022-01-17 18:14:54 -0300

    ✨ feature: splash screen

--- app/src/main/java/com/michelle/todolist/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/michelle/todolist/ui/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.michelle.todolist.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.michelle.todolist.R
 import com.michelle.todolist.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -18,6 +21,11 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_taskFragment)
+        }, 1000)
+
         return binding.root
     }
 }
