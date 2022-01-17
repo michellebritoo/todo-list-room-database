e6e3e8acce08878d571435abdbaa2c31ccb1c9c0
michellebritoo <michele.brito17@gmail.com>
2022-01-17 20:24:50 -0300

    🚧 Add a taskRepository interface and implements with DataBaseDataSource

---
+++ app/src/main/java/com/michelle/todolist/data/repository/DataBaseDataSource.kt
@@ -0,0 +1,33 @@
+package com.michelle.todolist.data.repository
+
+import com.michelle.todolist.data.db.dao.TaskDAO
+import com.michelle.todolist.data.db.entity.TaskEntity
+
+class DataBaseDataSource(
+    private val taskDAO: TaskDAO
+) : TaskRepository {
+    override suspend fun insertTask(title: String, description: String): Long {
+        return taskDAO.insertTask(
+            TaskEntity(
+                title = title,
+                description = description
+            )
+        )
+    }
+
+    override suspend fun updateTask(id: Long, title: String, description: String) {
+        taskDAO.updateTask(
+            TaskEntity(
+                title = title,
+                description = description
+            )
+        )
+    }
+
+    override suspend fun deleteTask(id: Long) = taskDAO.deleteTask(id)
+
+    override suspend fun deleteAllTasks() = taskDAO.deleteAllTasks()
+
+    override suspend fun getAllTasks() = taskDAO.getAllTasks()
+
+}
---
+++ app/src/main/java/com/michelle/todolist/data/repository/TaskRepository.kt
@@ -0,0 +1,16 @@
+package com.michelle.todolist.data.repository
+
+import androidx.lifecycle.LiveData
+import com.michelle.todolist.data.db.entity.TaskEntity
+
+interface TaskRepository {
+    suspend fun insertTask(title: String, description: String): Long
+
+    suspend fun updateTask(id: Long, title: String, description: String)
+
+    suspend fun deleteTask(id: Long)
+
+    suspend fun deleteAllTasks()
+
+    suspend fun getAllTasks(): LiveData<List<TaskEntity>>
+}
