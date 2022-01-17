2c282ac033d4424615d3dc490cbc66714aa545a5
michellebritoo <michele.brito17@gmail.com>
2022-01-17 18:25:39 -0300

    🚧 Update task fragment layout - using animations with material text field

--- app/src/main/java/com/michelle/todolist/ui/task/TaskFragment.kt
+++ app/src/main/java/com/michelle/todolist/ui/task/TaskFragment.kt
@@ -8,14 +8,17 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import com.michelle.todolist.R
+import com.michelle.todolist.databinding.TaskFragmentBinding
 
 class TaskFragment : Fragment() {
     private val viewModel: TaskViewModel by viewModels()
+    private lateinit var binding: TaskFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.task_fragment, container, false)
+    ): View {
+        binding = TaskFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
